370e5b4c985b9fece443de38179bf232ded25b71
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-11-27 17:43:17 +0100

    Main changes

--- ilovesessions/src/lib/components/Header.svelte
+++ ilovesessions/src/lib/components/Header.svelte
@@ -2,32 +2,33 @@
 import { FdndLogo } from '@fdnd/components';
 </script>
     
-<header class="header">
+<header>
   <FdndLogo />
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
   <ul class="menu">
-    <li><a href="#about">About</a></li>
+    <li><a href="/">Home</a></li>
   </ul>
 </header>
     
 
 <style>
-.header {
+header {
   box-shadow: 1px 1px 4px 0 rgba(0,0,0,.1);
   position: sticky;
   width: 100%;
   z-index: 3;
+  padding: 1em;
 }
 
-.header ul {
+header ul {
   margin: 0;
   padding: 0;
   list-style: none;
   overflow: hidden;
 }
 
-.header li a {
+header li a {
   display: block;
   padding: 20px 20px;
   text-decoration: none;
@@ -36,7 +37,7 @@ import { FdndLogo } from '@fdnd/components';
 
 /* menu */
 
-.header .menu {
+header .menu {
   clear: both;
   max-height: 0;
   transition: max-height .2s ease-out;
@@ -44,16 +45,15 @@ import { FdndLogo } from '@fdnd/components';
 
 /* menu icon */
 
-.header .menu-icon {
+header .menu-icon {
   cursor: pointer;
   display: inline-block;
-  float: right;
   padding: 28px 20px;
   position: relative;
   user-select: none;
 }
 
-.header .menu-icon .navicon {
+header .menu-icon .navicon {
   background: #333;
   display: block;
   height: 2px;
@@ -62,8 +62,8 @@ import { FdndLogo } from '@fdnd/components';
   width: 18px;
 }
 
-.header .menu-icon .navicon:before,
-.header .menu-icon .navicon:after {
+header .menu-icon .navicon:before,
+header .menu-icon .navicon:after {
   background: #333;
   content: '';
   display: block;
@@ -73,56 +73,56 @@ import { FdndLogo } from '@fdnd/components';
   width: 100%;
 }
 
-.header .menu-icon .navicon:before {
+header .menu-icon .navicon:before {
   top: 5px;
 }
 
-.header .menu-icon .navicon:after {
+header .menu-icon .navicon:after {
   top: -5px;
 }
 
 /* menu btn */
 
-.header .menu-btn {
+header .menu-btn {
   display: none;
 }
 
-.header .menu-btn:checked ~ .menu {
+header .menu-btn:checked ~ .menu {
   max-height: 240px;
 }
 
-.header .menu-btn:checked ~ .menu-icon .navicon {
+header .menu-btn:checked ~ .menu-icon .navicon {
   background: transparent;
 }
 
-.header .menu-btn:checked ~ .menu-icon .navicon:before {
+header .menu-btn:checked ~ .menu-icon .navicon:before {
   transform: rotate(-45deg);
 }
 
-.header .menu-btn:checked ~ .menu-icon .navicon:after {
+header .menu-btn:checked ~ .menu-icon .navicon:after {
   transform: rotate(45deg);
 }
 
-.header .menu-btn:checked ~ .menu-icon:not(.steps) .navicon:before,
-.header .menu-btn:checked ~ .menu-icon:not(.steps) .navicon:after {
+header .menu-btn:checked ~ .menu-icon:not(.steps) .navicon:before,
+header .menu-btn:checked ~ .menu-icon:not(.steps) .navicon:after {
   top: 0;
 }
 
 /* 48em = 768px */
 
 @media (min-width: 48em) {
-  .header li {
+  header li {
     float: left;
   }
-  .header li a {
+  header li a {
     padding: 20px 30px;
   }
-  .header .menu {
+  header .menu {
     clear: none;
     float: right;
     max-height: none;
   }
-  .header .menu-icon {
+  header .menu-icon {
     display: none;
   }
 }
--- ilovesessions/src/routes/aimasterclass/+page.svelte
+++ ilovesessions/src/routes/aimasterclass/+page.svelte
@@ -8,24 +8,24 @@
 <main>
   <article>
     <div class="site-wrap">
-	  <h1>Fatih's talk over AI in front end</h1>
+      <h1>Fatih's talk over AI in front end</h1>
       <h3>Finally, an a AI blog by Fatih. </h3>
       
       <p>Met deze blog zal ik een ontwikkeling binnen front end genaamd AI behandelen. Ik zal vertellen wat AI is en daarnaast me persoonlijke ervaring benoemen</p>
       
       <h3>De internationale ontwikkeling van AI binnen front-end</h3>
-      <p>Kunstmatige intelligentie (AI) is een snel evoluerende technologie die een steeds grotere impact heeft op alle aspecten van ons leven, inclusief de ontwikkeling van webdev. AI wordt steeds vaker gebruikt om front-end applicaties te verbeteren, door taken te automatiseren, gebruikerservaringen te verbeteren en nieuwe mogelijkheden te creëren.</p>
+      <p>Kunstmatige intelligentie (AI) is een snel ontwikkelend technologie die een steeds grotere impact heeft op alle aspecten van ons leven, inclusief de ontwikkeling van webdev. AI wordt steeds vaker gebruikt om front-end apps te verbeteren, door taken te automatiseren, gebruikerservaringen te verbeteren en nieuwe mogelijkheden te creëren. Je kan doormiddel van AI Codes genereren, codes valideren en ook refactoren</p>
 
       <h3>Internationale trends</h3>
-      <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten is er bijvoorbeeld een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webtoepassingen. Ook in Europa is er een groeiende interesse in AI binnen front-end. Dit komt onder meer doordat de Europese Commissie AI als een belangrijke technologie ziet voor de digitale transformatie. De Commissie heeft daarom verschillende initiatieven genomen om de ontwikkeling van AI in Europa te stimuleren.</p>
+      <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten is er bijvoorbeeld een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webapps. Ook in Europa is er een groeiende interesse in AI binnen front-end. Dit komt onder meer doordat de Europese Commissie AI als een belangrijke technologie ziet voor de digitale verandering. De Commissie heeft daarom verschillende initiatieven genomen om de ontwikkeling van AI in Europa te stimuleren.</p>
 
       <h3>Concreet gebruik van AI binnen front-end</h3>
       <p>AI wordt op verschillende manieren gebruikt binnen front-end applicaties. Enkele voorbeelden zijn:</p>
 
         <ul>
           <li>Automatisering van taken: AI kan worden gebruikt om taken te automatiseren die momenteel door front-end developers worden uitgevoerd. Dit kan bijvoorbeeld gaan om het genereren van code, het testen van applicaties of het analyseren van gebruikersgegevens.</li>
-          <li>Verbetering van de gebruikerservaring: AI kan worden gebruikt om de gebruikerservaring van webtoepassingen te verbeteren. Dit kan bijvoorbeeld gaan om het personaliseren van content, het voorspellen van gebruikersacties of het bieden van assistentie.</li>
-          <li>Creatie van nieuwe mogelijkheden: AI kan worden gebruikt om nieuwe mogelijkheden te creëren voor webtoepassingen. Dit kan bijvoorbeeld gaan om het creëren van realistische 3D-weergaves, het genereren van kunstmatige intelligentie of het creëren van chatbots.</li>
+          <li>Verbetering van de gebruikerservaring: AI kan worden gebruikt om de gebruikerservaring van webapps te verbeteren. Dit kan bijvoorbeeld gaan om het personaliseren van content, het voorspellen van gebruikersacties(zoals bij vs code bijv) of het bieden van assistentie.</li>
+          <li>Creatie van nieuwe mogelijkheden: AI kan worden gebruikt om nieuwe mogelijkheden te creëren voor webapsp. Dit kan bijvoorbeeld gaan om het creëren van realistische 3D-weergaves, het genereren van kunstmatige intelligentie of het creëren van chatbots.</li>
         </ul>
 
         <h3>Persoonlijke ervaring</h3>
@@ -38,19 +38,20 @@
         <h3>Toekomstperspectieven</h3>
         <p>De ontwikkeling van AI binnen front-end zal in de toekomst naar verwachting verder versnellen. Dit komt doordat AI-technologieën steeds krachtiger en toegankelijker worden. Hierdoor zullen AI-toepassingen in de toekomst steeds vaker deel uitmaken van onze dagelijkse interactie met het web. Front-end developers die willen blijven concurreren, zullen zich moeten blijven ontwikkelen op het gebied van AI. Dit betekent dat ze kennis moeten opdoen van AI-principes en -technologieën. Ook moeten ze in staat zijn om AI-toepassingen te integreren in hun front-end applicaties.</p>
 
-
         <h3>Conclusie</h3>
 
-        <p>Uit Fatih's talk over AI in front-end kunnen we concluderen dat AI een belangrijke rol gaat spelen in de ontwikkeling van webtoepassingen. AI wordt al op verschillende manieren gebruikt om front-end applicaties te verbeteren, zoals door taken te automatiseren, de gebruikerservaring te verbeteren en nieuwe mogelijkheden te creëren.</p>
+        <p>Uit mijn talk over AI in front-end kunnen we concluderen dat AI een belangrijke rol gaat spelen in de ontwikkeling van webapps. AI wordt al op verschillende manieren gebruikt om front-end applicaties te verbeteren, zoals door taken te automatiseren, de gebruikerservaring te verbeteren en nieuwe mogelijkheden te creëren.</p>
           
-        <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten en Europa is er een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webtoepassingen.</p>
+        <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten en Europa is er een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webapps.</p>
           
         <p>De toekomstperspectieven voor AI in front-end zijn positief. AI-technologieën worden steeds krachtiger en toegankelijker, waardoor AI-toepassingen in de toekomst steeds vaker deel uit zullen maken van onze dagelijkse interactie met het web.</p>
 
-        <p>Wel 1 ding wat heel belangrijk is. AI zal een grote rol spelen maar het kan iemand lui maken. Er kan zelfs een kans komen dat meerendeel door middel van ai gemaakt zal worden. Bijvoorbeeld deze blog bestaat 40% uit Bard gegeneerd data.</p>
+        <p>Wel 1 ding wat heel belangrijk is. AI zal een grote rol spelen maar het kan iemand lui maken. Er kan zelfs een kans komen dat meerendeel door middel van AI gemaakt zal worden. Bijvoorbeeld deze blog bestaat 40% uit Bard gegeneerd data.</p>
 
-
-        <p>bron: BARD</p>
+        <p>bron:</p>
+        <p>Bard AI</p>
+        <p>https://www.youtube.com/watch?v=p6Yw0Bx5dbw</p>
+        <p>https://www.youtube.com/watch?v=xW9DJTvB3NI</p>
   </article>
 </main>
 
