8e6e748e2da6663630add806e5edce5cfcdb2a35
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-11-27 16:53:00 +0100

    Conclusie

--- ilovesessions/src/routes/aimasterclass/+page.svelte
+++ ilovesessions/src/routes/aimasterclass/+page.svelte
@@ -5,52 +5,54 @@
 
 <Header />
 
-<h1 class="header-title">Fatih's talk over AI in front end</h1>
-
 <main>
-    <article>
-              <h1>AI Revolutioneert Front-End Ontwikkeling</h1>
-              <p>De wereld van front-end ontwikkeling is ingrijpend veranderd door de opkomst van artificial intelligence (AI) tools zoals ChatGPT en Bard.
-                Deze geavanceerde AI-modellen stellen front-end ontwikkelaars in staat om workflows te stroomlijnen, gebruikerservaringen te verbeteren en creatieve mogelijkheden uit te breiden.</p>
+  <article>
+    <div class="site-wrap">
+	  <h1>Fatih's talk over AI in front end</h1>
+      <h3>Finally, an a AI blog by Fatih. </h3>
+      
+      <p>Met deze blog zal ik een ontwikkeling binnen front end genaamd AI behandelen. Ik zal vertellen wat AI is en daarnaast me persoonlijke ervaring benoemen</p>
+      
+      <h3>De internationale ontwikkeling van AI binnen front-end</h3>
+      <p>Kunstmatige intelligentie (AI) is een snel evoluerende technologie die een steeds grotere impact heeft op alle aspecten van ons leven, inclusief de ontwikkeling van webdev. AI wordt steeds vaker gebruikt om front-end applicaties te verbeteren, door taken te automatiseren, gebruikerservaringen te verbeteren en nieuwe mogelijkheden te creëren.</p>
+
+      <h3>Internationale trends</h3>
+      <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten is er bijvoorbeeld een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webtoepassingen. Ook in Europa is er een groeiende interesse in AI binnen front-end. Dit komt onder meer doordat de Europese Commissie AI als een belangrijke technologie ziet voor de digitale transformatie. De Commissie heeft daarom verschillende initiatieven genomen om de ontwikkeling van AI in Europa te stimuleren.</p>
+
+      <h3>Concreet gebruik van AI binnen front-end</h3>
+      <p>AI wordt op verschillende manieren gebruikt binnen front-end applicaties. Enkele voorbeelden zijn:</p>
+
+        <ul>
+          <li>Automatisering van taken: AI kan worden gebruikt om taken te automatiseren die momenteel door front-end developers worden uitgevoerd. Dit kan bijvoorbeeld gaan om het genereren van code, het testen van applicaties of het analyseren van gebruikersgegevens.</li>
+          <li>Verbetering van de gebruikerservaring: AI kan worden gebruikt om de gebruikerservaring van webtoepassingen te verbeteren. Dit kan bijvoorbeeld gaan om het personaliseren van content, het voorspellen van gebruikersacties of het bieden van assistentie.</li>
+          <li>Creatie van nieuwe mogelijkheden: AI kan worden gebruikt om nieuwe mogelijkheden te creëren voor webtoepassingen. Dit kan bijvoorbeeld gaan om het creëren van realistische 3D-weergaves, het genereren van kunstmatige intelligentie of het creëren van chatbots.</li>
+        </ul>
+
+        <h3>Persoonlijke ervaring</h3>
+        <p>Mijn persoonlijke ervaring met AI voor front-end is gemengd. Ik heb met behulp van GPT bijvoorbeeld een heel Sveltekit-project en React-webapp kunnen bouwen. Naast de codes die ik ontving, kreeg ik ook een uitleg over wat elk stukje code doet, waardoor ik er ook van kan leren.</p>
+
+        <p>Naast het idee dat het je goed kan helpen, kan je wel te maken hebben met tijd. Sommige basiscodes kan GPT zonder een correcte context voor jou moeilijk maken om tot een snelle oplossing te komen. Ik heb soms meegemaakt dat ik bijvoorbeeld iets van 5 minuten werk pas na een uurtje werkend kreeg met GPT.</p>
           
-              <h2>Vereenvoudiging van repetitieve taken</h2>
-              <p>Een van de belangrijkste gevolgen van AI in front-end ontwikkeling is het vermogen om repetitieve taken te automatiseren.
-                AI-algoritmen kunnen efficiënt omgaan met alledaagse taken zoals het genereren van boilerplatecode, het formatteren van tekst en het optimaliseren van afbeeldingen.
-                Deze automatisering geeft ontwikkelaars tijd vrij om zich te concentreren op complexere en creatieve aspecten van hun projecten.</p>
+        <p>Je moet soms ook oppassen met wat je copy-pastet, want soms krijg je bijvoorbeeld stukjes CSS die je helemaal niet nodig hebt. Maar is het handig om het te hebben in het leven? Ja, zeker weten. GPT is voor mij een echte aanwinst voor developers, gezien het je kan helpen met coderen en ook nog een sparringspartner kan zijn. Je kan je code er in stoppen en dan GPT laten kijken of je code klopt of of het anders kan. Je kan zelf refactoren!</p>
+
+        <h3>Toekomstperspectieven</h3>
+        <p>De ontwikkeling van AI binnen front-end zal in de toekomst naar verwachting verder versnellen. Dit komt doordat AI-technologieën steeds krachtiger en toegankelijker worden. Hierdoor zullen AI-toepassingen in de toekomst steeds vaker deel uitmaken van onze dagelijkse interactie met het web. Front-end developers die willen blijven concurreren, zullen zich moeten blijven ontwikkelen op het gebied van AI. Dit betekent dat ze kennis moeten opdoen van AI-principes en -technologieën. Ook moeten ze in staat zijn om AI-toepassingen te integreren in hun front-end applicaties.</p>
+
+
+        <h3>Conclusie</h3>
+        <p>Conclusie</p>
+
+        <p>Uit Fatih's talk over AI in front-end kunnen we concluderen dat AI een belangrijke rol gaat spelen in de ontwikkeling van webtoepassingen. AI wordt al op verschillende manieren gebruikt om front-end applicaties te verbeteren, zoals door taken te automatiseren, de gebruikerservaring te verbeteren en nieuwe mogelijkheden te creëren.</p>
           
-              <h2>Verbeteren van gebruikerservaringen</h2>
-              <p>AI speelt ook een cruciale rol bij het creëren van meer gepersonaliseerde en boeiende gebruikerservaringen.
-                AI-gestuurde tools kunnen gebruikersgedrag en voorkeuren analyseren om websiteontwerpen, content aanbevelingen en interactieve elementen op maat aan te passen.
-                Deze gepersonaliseerde aanpak verhoogt de tevredenheid van gebruikers en moedigt ze aan om meer betrokken te raken bij de website.</p>
+        <p>De ontwikkeling van AI binnen front-end is wereldwijd zichtbaar. In de Verenigde Staten en Europa is er een toenemende vraag naar AI-competente front-end developers. Dit komt doordat steeds meer bedrijven AI-technologieën willen implementeren in hun webtoepassingen.</p>
           
-              <h2>Uitbreiden van creatieve mogelijkheden</h2>
-              <p>AI opent nieuwe mogelijkheden voor creativiteit in front-end ontwikkeling.
-                AI-modellen kunnen innovatieve ontwerpconcepten genereren, unieke visuele elementen produceren en zelfs boeiende teksten componeren.
-                Deze infusie van AI-creativiteit stelt ontwikkelaars in staat om de grenzen van traditionele front-end ontwikkeling te verleggen en echt baanbrekende digitale ervaringen te creëren.</p>
-          
-              <h2>Voorbeelden van AI-toepassingen in front-end ontwikkeling</h2>
-              <ul>
-                     <li>ChatGPT-gestuurde chatbots kunnen realtime klantondersteuning bieden, gebruikersvragen beantwoorden en zelfs transacties faciliteren.</li>
-                     <li>Bard-gestuurde ontwerptools kunnen kleurenpaletten suggereren, typografiestijl genereren en lay-outopties aanbevelen op basis van gebruikersvoorkeuren.</li>
-                     <li>AI-gestuurde codegeneratoren kunnen codesnippets voor specifieke taken produceren, waardoor ontwikkelaars tijd en moeite besparen bij het schrijven van repetitieve code.</li>
-              </ul>
-          
-              <h2>Conclusie</h2>
-              <p>De integratie van AI in front-end ontwikkeling markeert een nieuw tijdperk van innovatie en efficiëntie. ChatGPT, Bard en andere AI-tools stellen ontwikkelaars in staat om meer gepersonaliseerde, boeiende en creatieve digitale ervaringen te creëren. Naarmate de AI-technologie zich blijft ontwikkelen, kunnen we nog meer transformerende toepassingen verwachten op het gebied van front-end ontwikkeling.</p>      
-    </article>
-    </main>
+        <p>De toekomstperspectieven voor AI in front-end zijn positief. AI-technologieën worden steeds krachtiger en toegankelijker, waardoor AI-toepassingen in de toekomst steeds vaker deel uit zullen maken van onze dagelijkse interactie met het web.</p>
+  </article>
+</main>
 
 <Footer />
 
 <style>
-
-.header-title {
-  text-align: center;
-  margin: 0 0 1rem 0;
-  font-size: 2rem;
-  font-weight: 600;
-}
-
 article {
   margin: 20px;
   padding: 40px;
@@ -58,35 +60,27 @@ article {
   font-family: sans-serif;
   line-height: 1.5;
 }
-
-h1 {
-  font-size: 24px;
-  margin-bottom: 10px;
+main {
+	/* Without this, the body has excess horizontal scroll when the menu is open */
+  overflow-x: hidden;
 }
 
-h2 {
-  font-size: 20px;
-  margin-bottom: 10px;
+/* Additional non-critical styles */
+
+h1, h3, p {
+  max-width: 600px;
+  margin: 0 auto 1em;
 }
 
-p {
-  font-size: 16px;
+/* Micro reset */
+*,*:before,*:after{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0;}
+main { height: 100%; width: 100%; font-family: Helvetica, Arial, sans-serif; }
+
+ul li {
+  max-width: 600px;
+  margin: 0 auto 1em;
+  font-family: sans-serif;
   line-height: 1.5;
-}
-
-img {
-  width: 100%;
-  height: auto;
-  margin-bottom: 10px;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  margin-bottom: 10px;
 }
 
 </style>
